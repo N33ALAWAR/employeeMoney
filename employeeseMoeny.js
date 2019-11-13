a309967c186e2380470345635916614fa31fb51f
n33alawar <nourhamza1987@gmail.com>
2019-11-13 13:36:45 +0100

    first commit

---
+++ employeeseMoeny.js
@@ -0,0 +1,60 @@
+// let currantMoeny = 100000;
+// let emplyeesNum = 0;
+// let nextSalarry = 1000;
+// let id = 1;
+// for (nextSalarry < currantMoeny)
+// i = workers
+// let id = [7500, 10000, 3000, 2000, 1500, 1000, 5000, 4000];
+// let maxmoney = 100000;
+// let lastPayed = 0;
+
+// for (let i = 0; i <= 72; i++) {
+// if ( maxmoney <= 0) {
+
+//     break;
+// }
+//   if (i < 1 && maxmoney >= 0) {
+//     maxmoney = maxmoney - id[0];
+//     lastPayed = id[0];
+//   } else if (i > 1 && i < 3) {
+//     maxmoney = maxmoney - id[1];
+//     lastPayed = id[1];
+//   } else if (i > 2 && i < 5) {
+//     maxmoney = maxmoney - id[2];
+//     lastPayed = id[2];
+//   } else if (i > 4 && i < 9) {
+//     maxmoney = maxmoney - id[3];
+//     lastPayed = id[3];
+//   } else if (i > 8 && i < 13) {
+//     maxmoney = maxmoney - id[4];
+//     lastPayed = id[4];
+//   } else if (i > 12 && i < 63) {
+//     maxmoney = maxmoney - id[5];
+//     lastPayed = id[5];
+//   } else if (i > 62 && i < 68) {
+//     maxmoney = maxmoney - id[6];
+//     lastPayed = id[6];
+//   } else if (i > 67 && i < 73) {
+//     maxmoney = maxmoney - id[7];
+//     lastPayed = id[7];
+//   }
+//   while (maxmoney < 100000) {
+//     maxmoney += lastPayed;
+//   }
+// }
+// console.log(maxmoney);
+// const word = "Racecar";
+// const halfLength = Math.floor(word.length / 2);
+// for (let i = 0; i < halfLength; i++) {
+//   const currentChar = word[i];
+//   const endOffsetChar = word[word.length - 1 - i];
+//   if (currentChar !== endOffsetChar) {
+//     console.log(false);
+//   } else {
+//     console.log(true);
+//   }
+// }
+var i;
+for (i = 2; i <= 20; i = 2 + 3) {
+  console.log(i);
+}
